41c088209f75b09bf29d17e882f9e8e9483707b1
PungwonLee <64236372+PungwonLee@users.noreply.github.com>
2021-12-26 17:26:16 +0900

    Add files via upload

---
+++ mfc_Drag Rect(사각형그리기2)/ChildView.cpp
@@ -0,0 +1,165 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication3-2.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_Lbutton = false;
+	m_select = false;
+
+}
+CChildView::~CChildView()
+{
+}
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+//	ON_WM_MOVE()
+	ON_WM_ERASEBKGND()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	CRect rect;
+	GetClientRect(rect);
+	int w = rect.Width();
+	int h = rect.Height();
+
+	CDC memDC;
+	memDC.CreateCompatibleDC(&dc);
+	CBitmap memBMP;
+	memBMP.CreateCompatibleBitmap(&dc, w, h);
+	memDC.SelectObject(memBMP);
+	memDC.Rectangle(rect);
+
+	auto it = m_rect.begin();
+	while (it != m_rect.end()) {
+		CBrush brush2(RGB(255, 255, 255));
+		memDC.SelectObject(brush2);
+		memDC.Rectangle((*it).m_st.x, (*it).m_st.y, (*it).m_en.x, (*it).m_en.y);
+
+		if ((*it).m_select == true) {
+			CBrush brush(RGB(255, 255, 0));
+			memDC.SelectObject(brush);
+			memDC.Rectangle((*it).m_st.x, (*it).m_st.y, (*it).m_en.x, (*it).m_en.y);
+		}
+		it++;
+	}
+
+	dc.BitBlt(0, 0, w, h, &memDC, 0, 0,SRCCOPY);
+}
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	m_Lbutton = true;
+	m_prev = point;
+	auto it = m_rect.rbegin();
+	while (it != m_rect.rend()) {
+		CRect rect((*it).m_st.x, (*it).m_st.y, (*it).m_en.x, (*it).m_en.y);
+		if (rect.PtInRect(point)) {
+			(*it).m_select = true;
+			m_select = true;
+			break;
+		}
+		it++;
+		Invalidate();
+	}
+
+	MyRect rect;
+	rect.m_st = point;
+	rect.m_en = point;
+	if (!m_select)
+		m_rect.push_back(rect);
+
+	Invalidate();
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	m_Lbutton = false;
+	
+	if (!m_select) {
+		m_rect.back().m_en = point;
+		if (m_rect.back().m_st.x > m_rect.back().m_en.x) {
+			int tmp = m_rect.back().m_st.x;
+			m_rect.back().m_st.x = m_rect.back().m_en.x;
+			m_rect.back().m_en.x = tmp;
+		}
+		if (m_rect.back().m_st.y > m_rect.back().m_en.y) {
+			int tmp = m_rect.back().m_st.y;
+			m_rect.back().m_st.y = m_rect.back().m_en.y;
+			m_rect.back().m_en.y = tmp;
+		}
+
+	}
+	auto it = m_rect.rbegin();
+	while (it != m_rect.rend()) {
+		(*it).m_select = 0;
+		it++;
+		Invalidate();
+	}
+
+	m_select = false;
+	Invalidate();
+	CWnd::OnLButtonUp(nFlags, point);
+}
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if (m_Lbutton&& !m_select) {
+		m_rect.back().m_en = point;
+		Invalidate();
+	}
+	auto it = m_rect.begin();
+	CPoint move = point;
+	move -= m_prev;
+	while (it != m_rect.end()) {
+		if ((*it).m_select == true) {
+			(*it).m_st += move;
+			(*it).m_en += move;
+		}
+		it++;
+		Invalidate();
+		m_prev = point;
+	}
+	Invalidate();
+	CWnd::OnMouseMove(nFlags, point);
+}
+
+BOOL CChildView::OnEraseBkgnd(CDC* pDC)
+{
+	return false;
+}
---
+++ mfc_Drag Rect(사각형그리기2)/ChildView.h
@@ -0,0 +1,51 @@
+﻿
+// ChildView.h: CChildView 클래스의 인터페이스
+//
+
+
+#pragma once
+#include<list>
+using namespace std;
+
+class MyRect {
+public:
+	CPoint m_st;
+	CPoint m_en;
+	bool m_select;//선택된 도형 체크용
+};
+// CChildView 창
+class CChildView : public CWnd
+{
+// 생성입니다.
+public:
+	CChildView();
+
+// 특성입니다.
+public:
+	list<MyRect> m_rect;
+	CPoint m_prev;   //클릭 좌표
+	bool m_Lbutton;
+	bool m_select;	//선택했는지?
+// 작업입니다.
+public:
+
+// 재정의입니다.
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// 구현입니다.
+public:
+	virtual ~CChildView();
+
+	// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+//	afx_msg void OnMove(int x, int y);
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
+
